4709c2ed10782c061236e0e9cc0fa4bff92179f5
Erland Isaksson <erland@isaksson.info>
2014-05-13 06:44:16 +0200

    Corrected issue with play actions in My Apps menu

--- src/main/plugin/BrowseManager.pm
+++ src/main/plugin/BrowseManager.pm
@@ -654,11 +654,9 @@ sub serviceItemMenu {
 										}else {
 								        	Plugins::IckStreamPlugin::ItemCache::setItemInCache($item->{'id'},$item);
 											$menu->{'play'} = 'ickstream://'.$item->{'id'};
-											if(defined($parent) && $parent->{'type'} ne 'album' && $parent->{'type'} ne 'playlist') {
-												$menu->{'type'} = 'audio';
-											}
-											$menu->{'on_select'} => 'play';
-											$menu->{'playall'} => 1;
+											$menu->{'type'} = 'audio';
+											$menu->{'on_select'} = 'play';
+											$menu->{'playall'} = 1;
 											if(defined($item->{'itemAttributes'}->{'album'}) && defined($item->{'itemAttributes'}->{'mainArtists'}) && defined($item->{'itemAttributes'}->{'mainArtists'}[0]) && ($parent->{'type'} ne 'album' || !defined($parent->{'id'}))) {
 												$menu->{'line1'} = $item->{'text'};
 												$menu->{'line2'} = $item->{'itemAttributes'}->{'mainArtists'}[0]->{'name'}." - ".$item->{'itemAttributes'}->{'album'}->{'name'};
